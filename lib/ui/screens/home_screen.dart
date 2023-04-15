df6798adccf9221ba5817b00aec032b5860a4507
Damian Krzemien <damian.krzem@gmail.com>
2023-04-15 08:49:11 +0200

    feat(login): login implementation (#1)
    Merge

---
+++ lib/ui/screens/home_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:fridger_app/ui/screens/login_screen.dart';
+import 'package:fridger_app/ui/widgets/home_widget.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeWidget(),
+    Text(
+      'Index 1: Fridge',
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    ),
+    Text(
+      'Index 2: Recipes',
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    ),
+    Text(
+      'Index 3: Shop List',
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    ),
+    Text(
+      'Index 4: Settings',
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fridger Alpha 0.0'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.food_bank),
+            label: 'Fridge',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book),
+            label: 'Recipes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_outlined),
+            label: 'Shop List',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        unselectedItemColor: Color.fromARGB(255, 104, 197, 87),
+        selectedItemColor: Color.fromARGB(255, 255, 194, 115),
+        onTap: _onItemTapped,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => LoginScreen(),
+            ),
+          );
+        },
+        child: Icon(Icons.login),
+      ),
+    );
+  }
+}
---
+++ lib/ui/screens/login_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:fridger_app/ui/screens/home_screen.dart';
+import 'package:fridger_app/ui/widgets/login_button_widget.dart';
+import 'package:fridger_app/ui/widgets/text_input_widget.dart';
+
+class LoginScreen extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fridger Alpha 0.0'),
+        automaticallyImplyLeading: false,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextInput(
+              labelText: 'Email',
+              hintText: 'Enter your email',
+              controller: emailController,
+            ),
+            SizedBox(height: 16),
+            TextInput(
+              labelText: 'Password',
+              hintText: 'Enter your password',
+              controller: passwordController,
+              obscureText: true,
+            ),
+            SizedBox(height: 16),
+            LoginButton(
+              text: 'Login',
+              onPressed: () {
+                // Perform login validation here
+                // For demo purposes, let's assume login is successful
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (_) => HomeScreen(),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
