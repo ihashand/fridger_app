86571d48f606357ea4fd084a96233389050ca462
Damian Krzemien <damian.krzem@gmail.com>
2023-04-16 11:40:34 +0200

    test

--- bin/git_hooks.dart
+++ bin/git_hooks.dart
@@ -1,10 +1,9 @@
-import "package:git_hooks/git_hooks.dart";
-import "dart:io";
+import 'package:git_hooks/git_hooks.dart';
+import 'dart:io';
 
 void main(List<String> arguments) {
   Map<Git, UserBackFun> params = {
     Git.prePush: prePush,
-    Git.preCommit: preCommit
   };
   List<String> args = List<String>.from(arguments);
   GitHooks.call(args, params);
@@ -23,17 +22,6 @@ Future<bool> prePush() async {
     ProcessResult testResult = await Process.run('flutter', ['test']);
     print(testResult.stdout);
     if (testResult.exitCode != 0) return false;
-  } catch (e) {
-    return false;
-  }
-  return true;
-}
-
-Future<bool> preCommit() async {
-  try {
-    ProcessResult result = await Process.run('dartanalyzer', ['bin']);
-    print(result.stdout);
-    if (result.exitCode != 0) return false;
   } catch (e) {
     return false;
   }
